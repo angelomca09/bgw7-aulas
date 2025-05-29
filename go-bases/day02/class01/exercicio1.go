867cd2f17f4eb5c20007bb42a8359d455e29933f
angelomca09 <angelomca09@gmail.com>
2025-05-29 14:14:24 -0300

    Go Bases Day 02 Class 01 All Exercises

---
+++ go-bases/day02/class01/exercicio1.go
@@ -0,0 +1,27 @@
+package class01
+
+import "fmt"
+
+func Exercicio1() {
+	fmt.Println("\nExercicio 1\n-----------------")
+
+	var salario float64
+	fmt.Print("Digite o salário do funcionário: ")
+	fmt.Scan(&salario)
+
+	var imposto, porcentagem = CalculaImposto(salario)
+
+	fmt.Printf("O imposto a ser pago é de R$%.2f, correspondente a %.0f%% do salário.\n", imposto, porcentagem*100)
+
+}
+
+func CalculaImposto(salario float64) (imposto, porcentagem float64) {
+	porcentagem = 0.0
+	if salario > 50000 {
+		porcentagem += 0.17
+	}
+	if salario > 150000 {
+		porcentagem += 0.10
+	}
+	return salario * porcentagem, porcentagem
+}
