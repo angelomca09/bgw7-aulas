3b5a7e36acb4164e3e4651ab62e936feb21b2af2
angelomca09 <angelomca09@gmail.com>
2025-05-29 13:38:07 -0300

    Go Bases Day 01 Class 01 All Exercises

---
+++ go-bases/day01/class01/ex01.go
@@ -0,0 +1,13 @@
+package class01
+
+import "fmt"
+
+func Exercicio1() {
+	fmt.Println("Exercício 1\n-----------------")
+
+	var nome string = "Angelo"
+	var endereco string = "Melicidade"
+
+	fmt.Println("Nome:", nome)
+	fmt.Println("Endereço:", endereco)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import aula01 "bgw7-aulas/go-bases/day01/class01"
+
+func main() {
+	aula01.Exercicio04()
+}
